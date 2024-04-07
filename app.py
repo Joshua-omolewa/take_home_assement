631c527d3e4ebe66af276d2eace8ceb970c96c6f
joshua-omolewa <omolewajoshua@gmail.com>
2024-04-06 23:11:43 -0600

    updating code for app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ api_end_date = "2023-12-03"
 
 # API restriction 
 offset = 0
-limit = 50
+limit = 5
 
 
 def extract_data(specific_date, kind, offset_value, limit_value):
@@ -95,6 +95,33 @@ def extract_data(specific_date, kind, offset_value, limit_value):
     
     return data_extracted
 
+
+def extract_book_data(book_id):
+    """
+    This function returns the data extracted in a list format from the API
+    based on specific book id (OLID)
+
+    book_id(str) : book olid extracted
+
+    return a list of data extracted
+    """
+    data_extracted = []
+
+    try:
+        logging.info(f"Connecting to API to extract book data {book_id}")
+
+        url = f"http://openlibrary.org/books/{book_id}.json"
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad responses
+
+        data = response.json()
+
+        data_extracted.append(data)  # Append extracted data
+        logging.info(f"Extract data from  API for book id {book_id}")
+    except requests.RequestException as e:
+        logging.error(f"Error fetching data from {url}: {str(e)}")
+    
+    return data_extracted
     
 
 #generate dates for data extraction using pandas
@@ -131,7 +158,7 @@ def extract_book(changes_list):
     Extract '/books/book_id' from the 'changes' column
 
     changes_list (dict): List of dictionaries containing changes
-    return: from '/books/book_id' return book_id or None
+    return: from '/books/book_id' return book_id (also know as OLID) or None
     """
     for change in changes_list:
         if '/books/' in change.get('key', ''):
@@ -155,7 +182,7 @@ if __name__ == "__main__":
     #     data[single_kind] = data_for_endpoint
 
     ## DATA EXTRACTION ##
-    ####### DATA EXTRACTION #######
+    ####### DATA EXTRACTION 1 #######
     extracted_data_frames = []  # To store DataFrames for each endpoint's data
 
     for single_date in dates:
@@ -174,6 +201,10 @@ if __name__ == "__main__":
     # Concatenate all DataFrames into a single DataFrame
     final_df = pd.concat(extracted_data_frames, ignore_index=True)
 
+    #changing datatype to timestamp for date columns
+    final_df['date'] = pd.to_datetime(df['date'])
+    final_df['timestamp'] = pd.to_datetime(df['timestamp'])
+
     ####### DATA TRANSFORMATION #######
 
     final_df2 = final_df.copy() # creating a copy of final dataframe
@@ -185,10 +216,49 @@ if __name__ == "__main__":
 
     final_df_filtered_copy.loc[:, 'book'] = final_df2['changes'].apply(extract_book) #adding a new column called books to contained extracted book id
 
-    print(final_df_filtered_copy)
+    # Drop rows with missing values only in specific columns
+    final_df_filtered_copy = final_df_filtered_copy.dropna(subset=['book'])
+
+    
+    final_df_filtered_copy .to_csv("table1.csv")
+
+    print(final_df_filtered_copy.columns)
+    print(final_df_filtered_copy.info())
 
 
-    # print(data)
+    ####### DATA EXTRACTION 2 #######
+
+    # Create an empty list to store extracted data
+    extracted_data2 = []
+
+    # Iterate over 'book' column in final_df_filtered_copy
+    for book_id in final_df_filtered_copy['book']:
+        # Extract data for each book ID
+        book_data = extract_book_data(book_id)
+        if book_data is not None:
+            # Extend extracted_data list with data for this book along with the book ID
+            for data in book_data:
+                data['book'] = book_id
+            extracted_data2.extend(book_data)
+
+
+    second_final_df  = pd.DataFrame(extracted_data2)
+    # print(second_final_df)
+    second_final_df.to_csv("table2_with_duplicate.csv")
+
+    # Drop duplicates based on one column
+    df_without_duplicates = second_final_df.drop_duplicates(subset=['book'])
+
+    # Drop rows with missing values only in specific columns
+    df_without_missing_values = df_without_duplicates.dropna(subset=['book'])
+
+    # print(df_without_duplicates)
+    df_without_duplicates.to_csv("table2_without_duplicate.csv")
+
+    print(df_without_missing_values.columns)
+    print(df_without_missing_values.info())
+
+
 
 
 # if __name__ == "__main__":
